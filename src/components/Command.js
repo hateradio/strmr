46b51ebcc69c995cfd64de9d2b510a08699fc68e
hateradio <radio.github@gmail.com>
2018-08-13 21:16:57 -0700

    modifies ytdl command; adds title case strings

--- src/components/Command.js
+++ src/components/Command.js
@@ -5,12 +5,20 @@ class Command {
 
     static pad = (num = 0) => num.toString().padStart(2, '0')
 
+    static lower = ['a', 'an', 'and', 'as', 'at', 'but', 'by', 'for', 'for', 'from', 'in', 'into', 'near', 'nor', 'of', 'on', 'onto', 'or', 'the', 'to', 'with']
+
+    static titleCase(word, index, last) {
+        if (index === 0 || index === last) return word
+
+        return Command.lower.find(s => s === word.toLowerCase()) || word
+    }
+
     /**
      * @param {string} name Escapes a title
      */
     static escape = name => slugify(name.replace(/['"]/g, ''))
         .split('-')
-        .map(s => s ? s[0].toUpperCase() + s.slice(1) : '')
+        .map((s, i, a) => s ? Command.titleCase(s[0].toUpperCase() + s.slice(1), i, a.length - 1) : '')
         .join('.')
 
     static showTitle = name => name && name.trim() ? `.${Command.escape(name)}` : ''
@@ -32,7 +40,7 @@ class Command {
             const { number, uri = '' } = ep
 
             let id = `S${Command.pad(season)}E${Command.pad(number)}`
-            return `# ${id}\nlivestreamer${Command.browser(browser)} "${protocol}://${Command.link(uri, protocol)}" "best" --hls-segment-threads ${threads} -f -o ${Command.escape(title)}.${id}${Command.showTitle(ep.title)}.${quality}.${extras}` //.replace(/(?:\w.m3u8)/g, 'i.m3u8')
+            return `# ${id}\nlivestreamer${Command.browser(browser)} '${protocol}://${Command.link(uri, protocol)}' 'best' --hls-segment-threads ${threads} -f -o ${Command.escape(title)}.${id}${Command.showTitle(ep.title)}.${quality}.${extras}` //.replace(/(?:\w.m3u8)/g, 'i.m3u8')
         })
 
         return commands
--- src/components/CommandYtdl.js
+++ src/components/CommandYtdl.js
@@ -11,7 +11,9 @@ class CommandYtdl {
 
             let subcmd = subs ? '--write-sub --sub-format ttml --convert-subtitles srt --embed-subs --merge-output-format mkv' : ''
 
-            const cmd = `# ${id}\nyoutube-dl -f "bestvideo[height<=${quality.replace('p', '')}]+bestaudio/best[height<=${quality.replace('p', '')}]" ${subcmd} --external-downloader aria2c --external-downloader-args "-c -j ${threads} -x ${threads} -s ${threads} -k 10M" --cookies cookies.txt ${uri} -o "${Command.escape(title)}.${id}${Command.showTitle(ep.title)}.${quality}.${extras}.%(ext)s"`
+            // --external-downloader aria2c --external-downloader-args "-c -j ${threads} -x ${threads} -s ${threads} -k 10M"
+
+            const cmd = `# ${id}\nyoutube-dl -f "bestvideo[height<=${quality.replace('p', '')}]+bestaudio/best[height<=${quality.replace('p', '')}]" ${subcmd} --cookies cookies.txt ${uri} -o "${Command.escape(title)}.${id}${Command.showTitle(ep.title)}.${quality}.${extras}.%(ext)s"`
 
             return cmd
         })
